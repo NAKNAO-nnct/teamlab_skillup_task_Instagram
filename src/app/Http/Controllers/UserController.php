9a6191ea8eb85610e33d35784a3ae12317066891
NAKNAO-nnct <totoro.windows@outlook.com>
2020-03-09 02:36:36 +0900

    個人ページなどの処理を追記

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use App\User;
+use App\Model\Bbs;
 use Auth;
 class UserController extends Controller
 {
@@ -11,5 +12,20 @@ class UserController extends Controller
 
         return var_dump($user->github_id);
         
+    }
+
+    // idのユーザ情報
+    public function userPageId($id) {
+        $user = User::where('github_id', $id)->get()[0];
+
+        $bbs = Bbs::where('github_id', $id)->latest()->get();
+
+        // いいねの総数
+        $good_sum = 0;
+        foreach ($bbs as $b) {
+            $good_sum += count(explode(',', $b->favorite_github_id)) - 1;
+        }
+
+        return view('user', ["user"=> $user, "bbs" => $bbs, "good" => $good_sum]);
     }
 }
