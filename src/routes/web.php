ebc7796cbe4cd75b95dc798eb018b1f1ba8ba806
NAKNAO-nnct <totoro.windows@outlook.com>
2020-03-09 02:37:24 +0900

    routingの設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/user', 'UserController@index');
+Route::get('github', 'Github\GithubController@top');
+
+// login
+// Route::post('/login', function(){});
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
+
+
+
+Route::get('/', 'BbsController@index');
+
+
+
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/upload', function(){
+        return view('upload');
+    });
+    // 投稿処理
+    Route::post('/upload', 'BbsController@upload');
 });
+
+// 記事
+Route::group(['prefix' => 'article'], function() {
+});
+// ログイン必須
+Route::group(['prefix' => 'article', 'middleware' => 'auth'], function() {
+    Route::get('{id}', 'BbsController@viewId');
+    
+    Route::get('{id}/good', 'BbsController@good');
+    Route::get('{id}/good/list', 'BbsController@goodUserList');
+
+    Route::get('{id}/delete', 'BbsController@delete');
+});
+
+// ユーザ
+Route::get('user/{id}', 'UserController@userPageId');
